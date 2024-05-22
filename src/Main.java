88592013b9a726db8c9ca4cb3c6970aa1dd1d85f
SistemaBancario_1 <mukito100@gmail.com>
2024-05-22 17:40:34 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+/*
+//Contador com for de 1 a 50
+
+public class Main {
+    public static void main(String[] args) {
+
+        for(int cont = 1; cont <= 50; cont++){
+            System.out.println("Contador: " + cont);
+        }
+    }
+}
+*/
+/*
+//contador com for de 50 a 1
+
+public class Main {
+    public static void main(String[] args) {
+
+        for(int cont = 50; cont >= 01; cont--){
+            System.out.println("Contador: " + cont);
+        }
+    }
+}
+*/
+
+/*
+// contador com while de 1 a 10
+
+public class Main {
+    public static void main(String[] args) {
+        int contador = 1;
+
+        while (contador <= 10){
+            System.out.println(contador);
+            contador++;
+        }
+    }
+}
+*/
+// contador com while de 10 a 1
+
+public class Main {
+    public static void main(String[] args) {
+        int contador = 10;
+
+        while (contador >= 1){
+            System.out.println(contador);
+            contador--;
+        }
+    }
+}
